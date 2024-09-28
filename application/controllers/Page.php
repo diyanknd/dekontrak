2fddb27bd7ec777ebb01fd368362dae8cdf5e1aa
diyanknd <diyanknd@gmail.com>
2024-09-28 17:11:51 +0800

    check login

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -4,6 +4,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Page extends CI_Controller
 {
 
+	public function __construct()
+    {
+        parent::__construct();
+        $this->db2 = $this->load->database('dekontrak', TRUE);
+        $this->check_login();
+    }
+
 	
 	public function index()
 	{
